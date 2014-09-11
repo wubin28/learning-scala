2d5c27c5061f507e753ace064830c1a0224c3711
Ben Wu <wubin28@gmail.com>
2014-09-12 00:09:06 +0800

    Extracted method linkStrings().

--- hello-scala/src/main/scala/StringsLinkerRunner.scala
+++ hello-scala/src/main/scala/StringsLinkerRunner.scala
@@ -4,12 +4,17 @@
 object StringsLinkerRunner {
   def main(args: Array[String]): Unit = {
     var bjdp = Array("Beijing", "Design", "Patterns", "Study", "Group")
+    var result: String = linkStrings(bjdp)
+    print(result)
+  }
+
+  def linkStrings(bjdp: Array[String]): String = {
     var i = 0
     var result = ""
     while (i < bjdp.length) {
       result += bjdp(i)
       i += 1
     }
-    print(result)
+    result
   }
 }
