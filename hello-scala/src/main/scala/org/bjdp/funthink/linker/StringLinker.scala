8a4d2fb9c489c6985b9c403dba1ed1740697cc97
Ben Wu <wubin28@gmail.com>
2014-09-12 01:04:22 +0800

    Made method StringLinker.linkStrings() more concise.

--- hello-scala/src/main/scala/org/bjdp/funthink/linker/StringLinker.scala
+++ hello-scala/src/main/scala/org/bjdp/funthink/linker/StringLinker.scala
@@ -4,7 +4,5 @@ package org.bjdp.funthink.linker
  * Created by ben on 9/12/14.
  */
 class StringLinker() {
-  def linkStrings(strings: Array[String]): String = {
-    strings.mkString
-  }
+  def linkStrings(strings: Array[String]) = strings.mkString
 }
