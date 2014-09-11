ff38a7552af92c0cd9f1b427f902b0aecfde1e1f
Ben Wu <wubin28@gmail.com>
2014-09-12 00:06:02 +0800

    Separated printing from result string generation in the main() method.

--- hello-scala/src/main/scala/StringsLinkerRunner.scala
+++ hello-scala/src/main/scala/StringsLinkerRunner.scala
@@ -5,9 +5,11 @@ object StringsLinkerRunner {
   def main(args: Array[String]): Unit = {
     var bjdp = Array("Beijing", "Design", "Patterns", "Study", "Group")
     var i = 0
+    var result = ""
     while (i < bjdp.length) {
-      print(bjdp(i))
+      result += bjdp(i)
       i += 1
     }
+    print(result)
   }
 }
